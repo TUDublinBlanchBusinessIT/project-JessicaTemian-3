457614757e04529476da116b59f50564cbbbd45b
JessicaTemian <B00153952@mytudublin.ie>
2025-04-14 12:15:05 +0000

    Updated loan form with book/member dropdowns and Bootstrap datepickers for borrow and return dates

--- resources/views/loans/fields.blade.php
+++ resources/views/loans/fields.blade.php
@@ -1,25 +1,25 @@
-<!-- Book Id Field -->
+<!-- Book Dropdown -->
 <div class="form-group col-sm-6">
-    {!! Form::label('book_id', 'Book Id:') !!}
-    {!! Form::number('book_id', null, ['class' => 'form-control']) !!}
+    {!! Form::label('book_id', 'Book:') !!}
+    {!! Form::select('book_id', \App\Models\Book::pluck('title', 'id'), null, ['class' => 'form-control', 'placeholder' => 'Select Book']) !!}
 </div>
 
-<!-- Member Id Field -->
+<!-- Member Dropdown -->
 <div class="form-group col-sm-6">
-    {!! Form::label('member_id', 'Member Id:') !!}
-    {!! Form::number('member_id', null, ['class' => 'form-control']) !!}
+    {!! Form::label('member_id', 'Member:') !!}
+    {!! Form::select('member_id', \App\Models\Member::pluck('name', 'id'), null, ['class' => 'form-control', 'placeholder' => 'Select Member']) !!}
 </div>
 
 <!-- Borrow Date Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('borrow_date', 'Borrow Date:') !!}
-    {!! Form::date('borrow_date', null, ['class' => 'form-control']) !!}
+    {!! Form::text('borrow_date', null, ['class' => 'form-control datepicker']) !!}
 </div>
 
 <!-- Return Date Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('return_date', 'Return Date:') !!}
-    {!! Form::date('return_date', null, ['class' => 'form-control']) !!}
+    {!! Form::text('return_date', null, ['class' => 'form-control datepicker']) !!}
 </div>
 
 <!-- Submit Field -->
