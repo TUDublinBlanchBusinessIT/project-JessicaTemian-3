99b8fae18b17d0f02f6a86ed582ba37eb74dc5a5
JessicaTemian <B00153952@mytudublin.ie>
2025-04-27 22:08:05 +0000

    Added datepicker for borrow and return dates

--- resources/views/loans/fields.blade.php
+++ resources/views/loans/fields.blade.php
@@ -1,29 +1,43 @@
 <!-- Book Dropdown -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6 mb-3">
     {!! Form::label('book_id', 'Book:') !!}
-    {!! Form::select('book_id', \App\Models\Book::pluck('title', 'id'), null, ['class' => 'form-control', 'placeholder' => 'Select Book']) !!}
+    {!! Form::select('book_id', \App\Models\Book::pluck('title', 'id'), null, [
+        'class' => 'form-control',
+        'placeholder' => 'Select a Book'
+    ]) !!}
 </div>
 
 <!-- Member Dropdown -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6 mb-3">
     {!! Form::label('member_id', 'Member:') !!}
-    {!! Form::select('member_id', \App\Models\Member::pluck('name', 'id'), null, ['class' => 'form-control', 'placeholder' => 'Select Member']) !!}
+    {!! Form::select('member_id', \App\Models\Member::pluck('name', 'id'), null, [
+        'class' => 'form-control',
+        'placeholder' => 'Select a Member'
+    ]) !!}
 </div>
 
 <!-- Borrow Date Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6 mb-3">
     {!! Form::label('borrow_date', 'Borrow Date:') !!}
-    {!! Form::text('borrow_date', null, ['class' => 'form-control datepicker']) !!}
+    {!! Form::text('borrow_date', null, [
+        'class' => 'form-control datepicker',
+        'autocomplete' => 'off',
+        'placeholder' => 'YYYY-MM-DD'
+    ]) !!}
 </div>
 
 <!-- Return Date Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6 mb-3">
     {!! Form::label('return_date', 'Return Date:') !!}
-    {!! Form::text('return_date', null, ['class' => 'form-control datepicker']) !!}
+    {!! Form::text('return_date', null, [
+        'class' => 'form-control datepicker',
+        'autocomplete' => 'off',
+        'placeholder' => 'YYYY-MM-DD'
+    ]) !!}
 </div>
 
 <!-- Submit Field -->
-<div class="form-group col-sm-12">
+<div class="form-group col-sm-12 mt-4">
     {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('loans.index') !!}" class="btn btn-default">Cancel</a>
+    <a href="{{ route('loans.index') }}" class="btn btn-secondary ms-2">Cancel</a>
 </div>
