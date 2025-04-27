00250644fdb882b616382ed5ebd2a45f97cfd74e
JessicaTemian <B00153952@mytudublin.ie>
2025-04-27 21:58:54 +0000

    Styled status badges on Books by green=availabe orange=borrowed

--- resources/views/books/table.blade.php
+++ resources/views/books/table.blade.php
@@ -11,24 +11,32 @@
     <tbody>
     @foreach($books as $book)
         <tr>
-            <td>{!! $book->title !!}</td>
-            <td>{!! $book->author !!}</td>
-            <td>{!! $book->genre->name ?? 'Unknown' !!}</td>
+            <td>{{ $book->title }}</td>
+            <td>{{ $book->author }}</td>
+            <td>{{ $book->genre->name ?? 'Unknown' }}</td>
             <td>{{ \Carbon\Carbon::parse($book->published_year)->format('Y') }}</td>
-            <td>{!! $book->condition !!}</td>
+            <td>{{ $book->condition }}</td>
             <td>
                 @if($book->is_available)
-                    <span class="badge badge-success">Available</span>
+                    <span class="badge bg-success">Available</span>
                 @else
-                    <span class="badge badge-danger">Borrowed</span>
+                    <span class="badge bg-warning text-dark">Borrowed</span>
                 @endif
             </td>
             <td>
                 {!! Form::open(['route' => ['books.destroy', $book->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
-                    <a href="{!! route('books.show', [$book->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-eye"></i></a>
-                    <a href="{!! route('books.edit', [$book->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-edit"></i></a>
-                    {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                    <a href="{{ route('books.show', $book->id) }}" class='btn btn-default btn-xs'>
+                        <i class="far fa-eye"></i>
+                    </a>
+                    <a href="{{ route('books.edit', $book->id) }}" class='btn btn-default btn-xs'>
+                        <i class="far fa-edit"></i>
+                    </a>
+                    {!! Form::button('<i class="far fa-trash-alt"></i>', [
+                        'type' => 'submit', 
+                        'class' => 'btn btn-danger btn-xs', 
+                        'onclick' => "return confirm('Are you sure?')"
+                    ]) !!}
                 </div>
                 {!! Form::close() !!}
             </td>
