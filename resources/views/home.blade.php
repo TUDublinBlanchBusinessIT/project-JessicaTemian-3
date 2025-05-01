07ecfcf57e6fa7679ad7cc1a23e7bb6b383ec4a5
JessicaTemian <B00153952@mytudublin.ie>
2025-05-01 12:23:34 +0000

    Redesigned my home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-5 text-center">
-    <h1 class="mb-4">📚 Welcome to BookBorrow</h1>
-    <p class="lead">Manage books, members, and loans efficiently.</p>
+<div class="container text-center py-5">
+    <div class="mb-5">
+        <h1 class="display-5">
+            <i class="fas fa-book-reader text-primary me-2"></i>Welcome to <strong>BookBorrow</strong>
+        </h1>
+        <p class="lead mt-3">Your simple solution for managing books, members, genres, and loans.</p>
+    </div>
 
-    <div class="mt-4">
-        <a href="{{ route('books.index') }}" class="btn btn-primary btn-lg m-2">
-            <i class="fas fa-book"></i> View Books
-        </a>
-        <a href="{{ route('members.index') }}" class="btn btn-secondary btn-lg m-2">
-            <i class="fas fa-users"></i> View Members
-        </a>
-        <a href="{{ route('loans.index') }}" class="btn btn-success btn-lg m-2">
-            <i class="fas fa-exchange-alt"></i> Manage Loans
-        </a>
-        <a href="{{ route('genres.index') }}" class="btn btn-info btn-lg m-2">
-            <i class="fas fa-list"></i> View Genres
-        </a>
+    <div class="row justify-content-center">
+        <div class="col-md-2 m-2">
+            <a href="{{ route('books.index') }}" class="btn btn-outline-primary btn-lg w-100">
+                <i class="fas fa-book me-2"></i>Books
+            </a>
+        </div>
+        <div class="col-md-2 m-2">
+            <a href="{{ route('members.index') }}" class="btn btn-outline-secondary btn-lg w-100">
+                <i class="fas fa-users me-2"></i>Members
+            </a>
+        </div>
+        <div class="col-md-2 m-2">
+            <a href="{{ route('loans.index') }}" class="btn btn-outline-success btn-lg w-100">
+                <i class="fas fa-random me-2"></i>Loans
+            </a>
+        </div>
+        <div class="col-md-2 m-2">
+            <a href="{{ route('genres.index') }}" class="btn btn-outline-info btn-lg w-100">
+                <i class="fas fa-tags me-2"></i>Genres
+            </a>
+        </div>
     </div>
 </div>
 @endsection
